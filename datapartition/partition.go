b227fc62ee8204844477d06124a734ce66ed6c0f
Anurag Kumar <anurag.hcu@gmail.com>
2023-05-03 23:23:22 +0530

    partition.go

--- datapartition/partition.go
+++ datapartition/partition.go
@@ -1,5 +1,30 @@
 package datapartition
 
-func Start(){
-	
+import (
+	"sync"
+)
+
+type KeyValueStore struct {
+	data  map[string]string
+	mutex sync.RWMutex
+}
+
+func (kv *KeyValueStore) Get(key string) (string, bool) {
+	kv.mutex.RLock()
+	defer kv.mutex.RUnlock()
+	value, ok := kv.data[key]
+	return value, ok
+}
+func (kv *KeyValueStore) Set(key string, value string) {
+	kv.mutex.Lock()
+	defer kv.mutex.Unlock()
+	kv.data[key] = value
+}
+
+type DataPartitions struct {
+	partitions []*KeyValueStore
+}
+
+func Start() {
+
 }
