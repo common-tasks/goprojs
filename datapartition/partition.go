82deee43c8134144b13be6fc6e34a22ff389a2ef
Anurag Kumar <anurag.hcu@gmail.com>
2023-05-18 22:46:17 +0530

    cleanup

--- datapartition/partition.go
+++ datapartition/partition.go
@@ -1,67 +1,41 @@
 package datapartition
 
 import (
-	"fmt"
 	"sync"
 )
 
-type KeyValueStore struct {
-	data  map[string]string
-	mutex sync.RWMutex
+/*
+To simulate the data partitioning
+*/
+
+type Store struct {
+	DataSet   map[int]string
+	storeLock sync.RWMutex
 }
 
-func (kv *KeyValueStore) Get(key string) (string, bool) {
-	kv.mutex.RLock()
-	defer kv.mutex.RUnlock()
-	value, ok := kv.data[key]
-	return value, ok
-}
-func (kv *KeyValueStore) Set(key string, value string) {
-	kv.mutex.Lock()
-	defer kv.mutex.Unlock()
-	kv.data[key] = value
+type Partitions struct {
+	store []Store
 }
 
-type DataPartitions struct {
-	partitions []*KeyValueStore
+func (s *Partitions) NumberOfStores() int {
+	count := len(s.store)
+	return count
 }
-
-func (dp *DataPartitions) GetPartition(key string) *KeyValueStore {
-	partition := dp.partitions[hashCode(key)%len(dp.partitions)]
-	return partition
+func (store *Store) Add(id int, data string) {
+	store.storeLock.RLocker().Lock()
+	defer store.storeLock.RLocker().Unlock()
+	store.DataSet[id] = data
 }
-func NewDataPartition(num int) *DataPartitions {
-	partitions := make([]*KeyValueStore, num)
-	for i := 0; i < num; i++ {
-		partitions[i] = &KeyValueStore{data: make(map[string]string)}
-	}
-	return &DataPartitions{partitions: partitions}
+func (store *Store) Get(id int) (string, bool) {
+	store.storeLock.RLock()
+	defer store.storeLock.RUnlock()
+	str, ok := store.DataSet[id]
+	return str, ok
 }
+func CreatePartitions(count int) *Partitions {
+	stores := make([]*Store, count)
 
-func hashCode(s string) int {
-	h := 0
-	for i := 0; i < len(s); i++ {
-		h = 31*h + int(s[i])
-	}
-	return h
-}
-
-func Start() {
-	dataPartition := NewDataPartition(3)
-
-	// Set keys
-	for i := 0; i < 10; i++ {
-		key := fmt.Sprintf("key%d", i)
-		value := fmt.Sprintf("value%d", i)
-		partition := dataPartition.GetPartition(key)
-		partition.Set(key, value)
-	}
-
-	// Get keys
-	for i := 0; i < 10; i++ {
-		key := fmt.Sprintf("key%d", i)
-		partition := dataPartition.GetPartition(key)
-		value, _ := partition.Get(key)
-		fmt.Printf("%s -> %s\n", key, value)
+	for i := 0; i < count; i++ {
+		stores[i]= &Store{DataSet: make(map[int]string)}
 	}
 }
