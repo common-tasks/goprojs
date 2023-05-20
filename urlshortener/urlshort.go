ba9c55b3e3a2dd23073126eebcd79b24ecc9c294
Anurag Kumar <anurag.hcu@gmail.com>
2023-05-20 14:14:46 +0530

    tests for short urlshort.go

--- urlshortener/urlshort.go
+++ urlshortener/urlshort.go
@@ -1,13 +1,53 @@
 package urlshortener
 
 import (
+	"fmt"
+	"math/rand"
 	"net/http"
+	"time"
 )
 
+var urlMap = make(map[string]string)
+//start function
 func Shorten() {
 	http.HandleFunc("/", redirectHandler)
+	http.HandleFunc("/shorten", shortURLHandler)
+	http.ListenAndServe(":8080", nil)
 }
 
 func redirectHandler(w http.ResponseWriter, r *http.Request) {
+	shorturl := r.URL.Path[1:]
+	url,ok:=urlMap[shorturl]
+	if(!ok){
+		http.Error(w,"Short url not found",http.StatusNotFound)
+		return
+	}
 
+	//redirect to original url
+	http.Redirect(w,r,url,http.StatusSeeOther)
+
+}
+func shortURLHandler(w http.ResponseWriter, r *http.Request) {
+	if r.Method != "POST" {
+		http.Error(w, "Invalid request method", http.StatusMethodNotAllowed)
+		return
+	}
+	url := r.FormValue("url")
+	if url == "" {
+		http.Error(w, "Missing url", http.StatusBadRequest)
+		return
+	}
+	shortURL := generateShortURL()
+	urlMap[shortURL] = url
+	fmt.Fprintf(w, "short url for url %s is %s\n", url, shortURL)
+}
+func generateShortURL() string {
+	var letters = []rune("abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789")
+	rand.Seed(time.Now().UnixNano())
+
+	b := make([]rune, 6)
+	for i := range b {
+		b[i] = letters[rand.Intn(len(letters))]
+	}
+	return string(b)
 }
--- urlshortener/urlshort_test.go
+++ urlshortener/urlshort_test.go
@@ -1,9 +1,42 @@
 package urlshortener
 
 import (
+	"net/http"
+	"net/http/httptest"
+	"net/url"
+	"strings"
 	"testing"
 )
 
-func TestShort(t *testing.T) {
-	Shorten()
+func TestShortenURLHandler(t *testing.T) {
+	req, err := http.NewRequest("POST", "/shorten", strings.NewReader(url.Values{"url": []string{"https://facebook.com/"}}.Encode()))
+	if err != nil {
+		t.Fatal(err)
+	}
+	req.Header.Add("Content-Type","application/x-www-form-urlencoded")
+	rr:=httptest.NewRecorder()
+	handler:=http.HandlerFunc(shortURLHandler)
+	handler.ServeHTTP(rr,req)
+	if status := rr.Code; status != http.StatusOK {
+		t.Errorf("handler returned wrong status code: got %v want %v", status, http.StatusOK)
+	}
+
+	expected := "Short URL is:"
+	if rr.Body.String()[:len(expected)] != expected {
+		t.Errorf("handler returned unexpected body: got %v want %v", rr.Body.String(), expected)
+	}
+}
+func TestGenerateShortURL(t *testing.T) {
+	url1 := generateShortURL()
+	url2 := generateShortURL()
+
+	if len(url1) != 6 {
+		t.Errorf("expected length 6 but got %d\n", len(url1))
+	}
+	if len(url2) != 6 {
+		t.Errorf("expected length 6 but got %d\n", len(url2))
+	}
+	if url1 == url2 {
+		t.Errorf("expected unique url but got %s and %s\n", url1, url2)
+	}
 }
