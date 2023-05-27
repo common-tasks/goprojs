313e7e1af6d7d7fe043f8e06427dfa1cf04dc5fb
Anurag Kumar <anurag.hcu@gmail.com>
2023-05-27 21:32:57 +0530

    polymorphism

--- concepts/concept.go
+++ concepts/concept.go
@@ -1,14 +1,43 @@
 package concepts
 
-type Cloud interface{
-	Connect(url string)bool
+type Cloud interface {
+	Connect(url string) bool
 	Disconnect() bool
-
 }
-type AzureCloud struct{
+type AzureCloud struct {
 	ClientID string
 	TenantID string
 }
-func TestIFace(){
 
+type AWS struct {
+	UserName string
+	Pwd      string
+	ID       string
+}
+
+func (azure AzureCloud) Connect(url string) bool {
+	return true
+}
+func (azure AzureCloud) Disconnect() bool {
+	return true
+}
+func (aws AWS) Connect(url string) bool {
+	return true
+}
+func (aws AWS) Disconnect() bool {
+	return true
+}
+func TestIFace() {
+	azure:=AzureCloud{ClientID: "99",TenantID: "12"}
+	ConnectToCloud(azure)
+	DisConnectCloud(azure)
+	aws:=AWS{UserName: "kk",Pwd: "**",ID: "xyz"}
+	ConnectToCloud(aws)
+	DisConnectCloud(aws)
+}
+func ConnectToCloud(cloud Cloud){
+	(cloud).Connect("")
+}
+func DisConnectCloud(cloud Cloud){
+	(cloud).Disconnect()
 }
