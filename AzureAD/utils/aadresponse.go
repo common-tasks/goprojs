c4c7fc829c197a32f1ae858804c348093b4ded67
Anurag Kumar <anurag.hcu@gmail.com>
2023-06-18 15:06:42 +0530

    cleanup.

--- AzureAD/utils/aadresponse.go
+++
@@ -1,8 +0,0 @@
-package utils
-
-type AuthResponse struct {
-	TokenType    string `json:"token_type"`
-	ExpiresIn    int    `json:"expires_in"`
-	ExtExpiresIn int    `json:"ext_expires_in"`
-	AccessToken  string `json:"access_token"`
-}
