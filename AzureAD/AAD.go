728abfe9d50c75f31b8f3f28b6790f55a6f0b724
Anurag Kumar <anurag.hcu@gmail.com>
2023-06-18 12:42:00 +0530

    code and test cleanup.

--- AzureAD/AAD.go
+++ AzureAD/AAD.go
@@ -1,32 +1,11 @@
 package azuread
 
 import (
-	"fmt"
 	"projects/AzureAD/groups"
 	"projects/AzureAD/token"
 	"projects/AzureAD/users"
 )
 
-func AzureAD() {
-	token, err := fetchToken()
-	if err != nil {
-		fmt.Printf("error in getting token %s", err)
-		return
-	}
-	// fmt.Printf("token is %s", token)
-
-	usrs, err := fetchUser(token)
-	if err != nil {
-		fmt.Printf("error in getting users %s", err)
-	}
-	fmt.Printf("users %d", len(usrs.Users))
-
-	grp, err := fetchGroups(token)
-	if err != nil {
-		fmt.Printf("error in getting groups %s", err)
-	}
-	fmt.Println("groups ",len(grp.Groups))
-}
 func fetchToken() (string, error) {
 	return token.FetchToken()
 }
--- AzureAD/AAD_test.go
+++ AzureAD/AAD_test.go
@@ -1,8 +1,43 @@
 package azuread
-import
-(
+
+import (
+	"fmt"
 	"testing"
 )
-func TestAzureAD(t *testing.T) {
-	AzureAD()
+func TestFetchToken(t *testing.T) {
+	_,err:=fetchToken()
+	if(err!=nil){
+		fmt.Printf("error is %s",err)
+	}
+	
 }
+
+func TestFetchUser(t *testing.T) {
+	token,err:=fetchToken()
+	if(err!=nil){
+		fmt.Printf("error in getting token %s",err)
+	}
+	resp,err:=fetchUser(token)
+	if(err!=nil){
+		fmt.Printf("error in getting user %s",err)
+	}
+	fmt.Printf("total number of users %d ",len(resp.Users))
+	for index, usr := range resp.Users {
+		fmt.Println(index,usr)
+	}
+}
+func TestFetchGroup(t *testing.T) {
+	token,err:=fetchToken()
+	if(err!=nil){
+		fmt.Printf("error in getting token %s",err)
+	}
+	resp,err:=fetchGroups(token)
+	if(err!=nil){
+		fmt.Printf("error in getting user %s",err)
+	}
+	fmt.Printf("total number of users %d ",len(resp.Groups))
+	for index, grp := range resp.Groups {
+		fmt.Println(index,grp)
+	}
+}
+
