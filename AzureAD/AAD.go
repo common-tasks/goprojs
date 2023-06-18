d822a7cb74f4297ecd6da30b3b66ddd1d97e34a5
Anurag Kumar <anurag.hcu@gmail.com>
2023-06-18 10:22:27 +0530

    AzureAD/utils/AzureConstant.go

--- AzureAD/AAD.go
+++ AzureAD/AAD.go
@@ -2,6 +2,7 @@ package azuread
 
 import (
 	"fmt"
+	"projects/AzureAD/groups"
 	"projects/AzureAD/token"
 	"projects/AzureAD/users"
 )
@@ -19,6 +20,12 @@ func AzureAD() {
 		fmt.Printf("error in getting users %s", err)
 	}
 	fmt.Printf("users %d", len(usrs.Users))
+
+	grp, err := fetchGroups(token)
+	if err != nil {
+		fmt.Printf("error in getting groups %s", err)
+	}
+	fmt.Println("groups ",len(grp.Groups))
 }
 func fetchToken() (string, error) {
 	return token.FetchToken()
@@ -26,4 +33,7 @@ func fetchToken() (string, error) {
 
 func fetchUser(token string) (*users.UserResponse, error) {
 	return users.FetchUser(token)
+}
+func fetchGroups(token string) (*groups.GroupResponse, error) {
+	return groups.FetchGroups(token)
 }
