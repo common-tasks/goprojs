add2ba3490983d1ee5c00113cbccc680ca95e92a
Anurag Kumar <anurag.hcu@gmail.com>
2023-07-01 23:17:17 +0530

    cc

--- myApp/api/handlers/userhandler.go
+++ myApp/api/handlers/userhandler.go
@@ -2,6 +2,7 @@ package handlers
 
 import (
 	"encoding/json"
+	"fmt"
 	"myapp/api/models"
 	"net/http"
 )
@@ -19,6 +20,7 @@ var users =[]models.User{
 
 func GetAllUsers(w http.ResponseWriter, r *http.Request){
 	if(r.URL.Path=="/user"){
+		fmt.Println("request at ",r.URL.Path)
 		data,err:=json.Marshal(users)
 		if(err!=nil){
 			http.Error(w,err.Error(),http.StatusInternalServerError)
